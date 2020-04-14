8660501fc1dabe814dc35a9c46a6868717cf3d46
Cola <milove@126.com>
2020-04-14 11:31:44 +0800

    update app res

--- web/js/downConfig2020.js
+++ web/js/downConfig2020.js
@@ -1,6 +1,6 @@
 // 官网安卓默认下载地址
 
-var config_an_url = 'https://cdn.jsdelivr.net/gh/colally/beauty/beauty_1.0_signed.apk';
+var config_an_url = 'http://beauty.qgfun.com/beauty_1.0_signed.apk';
 
 // 是否开启苹果应用市场
 var config_isOn	= false;
